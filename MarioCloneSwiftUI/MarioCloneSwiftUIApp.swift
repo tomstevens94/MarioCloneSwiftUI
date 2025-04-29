d13076e4a49765fe2976c9833db34de228582059
Thomas Stevens <tommstevens94@gmail.com>
2025-04-29 21:23:13 +0100

    Initial Commit

---
+++ MarioCloneSwiftUI/MarioCloneSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarioCloneSwiftUIApp.swift
+//  MarioCloneSwiftUI
+//
+//  Created by Tom Stevens on 29/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct MarioCloneSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MarioCloneSwiftUITests/MarioCloneSwiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  MarioCloneSwiftUITests.swift
+//  MarioCloneSwiftUITests
+//
+//  Created by Tom Stevens on 29/04/2025.
+//
+
+import Testing
+@testable import MarioCloneSwiftUI
+
+struct MarioCloneSwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
