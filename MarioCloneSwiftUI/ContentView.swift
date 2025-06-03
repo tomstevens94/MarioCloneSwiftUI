8ff89e25a2eb4e39b574a55a88b473111e4ef0d9
Thomas Stevens <tommstevens94@gmail.com>
2025-06-03 20:52:21 +0100

    ContenView + ViewModel

--- MarioCloneSwiftUI/ContentView.swift
+++ MarioCloneSwiftUI/ContentView.swift
@@ -8,14 +8,59 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    let viewModel = ViewModel();
+
+    let gridHeight = 15;
+    let screenHeight = UIScreen.main.bounds;
+    
+    let spriteScale: CGFloat = 2.0;
+    
+    func drawSprite(_ spriteName: SpriteName, at position: CGPoint, context: GraphicsContext) -> Void {
+        let sprite = viewModel.sprites[spriteName];
+        guard let sprite = sprite else { return };
+        
+        context.draw(
+            Image(decorative: sprite.image, scale: 1.0).interpolation(.none),
+            at: position,
+            anchor: .zero
+        )
+    }
+    
+    func executeDrawCommands(_ drawCommands: [DrawCommand], on context: GraphicsContext) -> Void {
+        for drawCommand in drawCommands {
+            switch drawCommand {
+            case .sprite(let spriteCommand):
+                drawSprite(
+                    spriteCommand.spriteName,
+                    at: spriteCommand.at,
+                    context: context
+                )
+            case .rect(let rectCommand):
+                context.stroke(
+                    Path(rectCommand.at),
+                    with: .color(rectCommand.color),
+                    lineWidth: 1
+                )
+            }
         }
-        .padding()
+    }
+    
+    var body: some View {
+        ZStack(alignment: .topLeading) {
+            Canvas() { context, size in
+                context.scaleBy(x: size.height / (12.0 * 16.0), y: size.height / (12.0 * 16.0))
+                
+                executeDrawCommands(viewModel.backgroundDrawCommands, on: context)
+                executeDrawCommands(viewModel.drawCommands, on: context)
+                executeDrawCommands(viewModel.debugDrawCommands, on: context)
+            }
+            
+            Button("Press") {
+                print("PRESSED")
+            }
+            .padding(32)
+        }
+        .ignoresSafeArea()
     }
 }
 
---
+++ MarioCloneSwiftUI/ViewModels/ContentViewViewModel.swift
@@ -0,0 +1,118 @@
+import SwiftUI
+
+struct Sprite {
+    let image: CGImage
+}
+
+enum SpriteName {
+    case marioIdle, ground, sky
+}
+
+enum SpriteImageName {
+    case Characters, Tiles
+}
+
+extension ContentView {
+    @Observable
+    
+    class ViewModel {
+        private(set) var spriteSourceImages = [SpriteImageName: CGImage]()
+        private(set) var sprites = [SpriteName: Sprite]()
+        
+        private(set) var timer: GameTimer
+        private(set) var ecsManager: ECSManager
+        private(set) var tileManager: TileManager
+        
+        var position = CGPoint(x: 20, y: 20)
+        var drawCommands: [DrawCommand] = []
+        var debugDrawCommands: [DrawCommand] = []
+        var backgroundDrawCommands: [DrawCommand] = []
+        
+        private func update(_ deltaTime: Double) {
+            ecsManager.update(deltaTime)
+            
+            drawCommands = ecsManager.drawCommands
+            debugDrawCommands = ecsManager.debugDrawCommands
+        }
+        
+        private func generateBackgroundDrawCommands() -> [DrawCommand] {
+            var commands: [DrawCommand] = []
+            
+            for x in 0..<30 {
+                for y in 0..<12 {
+                    commands.append(
+                        .sprite(
+                            SpriteDrawCommand(
+                                spriteName: .sky,
+                                at: .init(x: Double(x) * 16.0, y: Double(y) * 16.0)
+                            )
+                        )
+                    )
+                }
+            }
+            
+            return commands
+        }
+        
+        init() {
+            self.timer = GameTimer()
+            
+            let tileManager = TileManager()
+            self.tileManager = tileManager
+            
+            self.ecsManager = ECSManager(tileManager: tileManager)
+            
+            backgroundDrawCommands = generateBackgroundDrawCommands()
+            
+            let mario = ecsManager.createEntity()
+            
+            ecsManager.addComponentToEntity(
+                PositionComponent(x: 110.0, y: 20.0),
+                to: mario
+            )
+            ecsManager.addComponentToEntity(
+                VelocityComponent(dx: 20, dy: 0),
+                to: mario
+            )
+            ecsManager.addComponentToEntity(
+                MassComponent(10.0),
+                to: mario
+            )
+            ecsManager.addComponentToEntity(
+                TileCollisionComponent(size: .init(width: 14, height: 16)),
+                to: mario
+            )
+            ecsManager.addComponentToEntity(
+                RenderComponent(spriteName: .marioIdle),
+                to: mario
+            )
+            
+            guard let charactersSourceImage = UIImage(named: "characters")?.cgImage,
+                let tilesSourceImage = UIImage(named: "tiles")?.cgImage  else { return }
+
+            spriteSourceImages[.Characters] = charactersSourceImage
+            spriteSourceImages[.Tiles] = tilesSourceImage
+            
+            self.defineSprite(.marioIdle, spriteImageName: .Characters, rect: CGRect(x: 276, y: 44, width: 14, height: 16))
+            self.defineSprite(.ground, spriteImageName: .Tiles, rect: CGRect(x: 0, y: 0, width: 16, height: 16))
+            self.defineSprite(.sky, spriteImageName: .Tiles, rect: CGRect(x: 48, y: 368, width: 16, height: 16))
+            
+            self.timer.start(update)
+        }
+        
+        func defineSprite(_ name: SpriteName, spriteImageName: SpriteImageName, rect: CGRect) {
+            guard let sourceImage = spriteSourceImages[spriteImageName],
+                  let cropped = sourceImage.cropping(to: rect) else { return }
+            
+            let sprite: CGImage = {
+                cropped
+            }()
+            
+            sprites[name] = .init(image: sprite);
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
