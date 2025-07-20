c94ee848b38ec5ab9113afd90b8e3fc140e43ab1
Tom Stevens <tommstevens94@gmail.com>
2025-07-20 16:35:38 +0100

    Full screen controls

--- MarioCloneSwiftUI/FullScreenControls.swift
+++ MarioCloneSwiftUI/FullScreenControls.swift
@@ -1,8 +1,40 @@
-//
-//  FullScreenControls.swift
-//  MarioCloneSwiftUI
-//
-//  Created by Tom Stevens on 20/07/2025.
-//
+import SwiftUI
 
-import Foundation
+struct FullScreenControls: View {
+    var inputManager: InputManager
+    
+    var body: some View {
+        HStack(spacing: 0) {
+            Color.clear
+                .contentShape(Rectangle())
+                .gesture(
+                    DragGesture(minimumDistance: 0)
+                        .onChanged{ _ in
+                            inputManager.beginInput(
+                                input: .left
+                            )
+                        }.onEnded{ _ in
+                            inputManager.endInput(
+                                input: .left
+                            )
+                        }
+                )
+            Color.clear
+                .contentShape(Rectangle())
+                .gesture(
+                    DragGesture(minimumDistance: 0)
+                        .onChanged{ _ in
+                            print("IN RIGHT")
+                            inputManager.beginInput(
+                                input: .right
+                            )
+                        }.onEnded{ _ in
+                            print("OUT RIGHT")
+                            inputManager.endInput(
+                                input: .right
+                            )
+                        }
+                )
+        }
+    }
+}
